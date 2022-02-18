611aa5b97fdf021ec7ea677e0922a6b7953b2d1f
Enes Gündüz <enesg1997@gmail.com>
2022-02-18 04:23:39 +0300

    Update
    Update

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="Quiz Question",fileName ="New Question")]
 public class QuestionSO : ScriptableObject
 {
-    
+    [TextArea(2,6)]
+    [SerializeField] string question = "Enter new question test here";
+    [SerializeField] string[] answers=new string[4];
+    [SerializeField] int correctAnswerIndex;
+    public string GetQuestion(){
+        return question;
+    }
+    public string GetAnswer(int index){
+        return answers[index];
+    }
+    public int GetCorrectAnswerIndex(){
+        return correctAnswerIndex;
+    }
+
 }
